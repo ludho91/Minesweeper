cfe77e852e62f5734f6ab1be96af073e83ee634f
Ludovic HO <ludovicho91@gmail.com>
2020-09-04 00:34:52 +0200

    Ajout d'une page d'option et de la page d'un partie

--- MinesweeperWPF/MainWindow.xaml.cs
+++ MinesweeperWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Minesweeper.Pages;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,41 @@ namespace Minesweeper
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region fields
+        /// <summary>
+        /// Page d'une partie
+        /// </summary>
+        private GamePage game;
+        private OptionsPage options;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
+            this.MainFrame.Source = new Uri("Pages/HomePage.xaml", UriKind.Relative);
+        }
+
+        /// <summary>
+        /// Handler appelé lors d'un click sur l'un des item du menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menu = ((MenuItem)e.Source);
+            if ( menu == NewGame)
+            {
+                if (game == null)
+                    game = GamePage.GamePageInstance;
+                MainFrame.Navigate(game);
+            }
+            if (menu == Options)
+            {
+                if (options == null)
+                    options = OptionsPage.OptionsPageInstance;
+                MainFrame.Navigate(options);
+            }
+                
+
         }
     }
 }
---
+++ MinesweeperWPF/Pages/GamePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minesweeper.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour GamePage.xaml
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        static GamePage()
+        {
+            
+        }
+
+        private GamePage()
+        {
+            InitializeComponent();
+        }
+
+        public static GamePage GamePageInstance { get; } = new GamePage();
+    }
+}
